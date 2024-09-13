f967e177ca51a96b47887c406b903019e93afab3
Nuhser <martinimmel@gmail.com>
2024-09-13 17:51:33 +0200

    #17 Improve migration script

---
+++ migrations.py
@@ -0,0 +1,69 @@
+import json
+import os
+import sys
+
+def migrateToV1_0_1(data):
+    tracker_entries: dict[str, list[dict]] = data["TrackerEntries"]
+    new_tracker_entries: dict[str, list[dict]] = dict()
+    
+    for date, entries in tracker_entries.items():
+        new_entries: dict[str, dict] = dict()
+
+        for entry in entries:
+            if (not entry["Name"] in new_entries):
+                new_entries[entry["Name"]] = {
+                    "Name": entry["Name"],
+                    "SubEntries": [
+                        {
+                            "Start": entry["Start"],
+                            "End": entry["End"]
+                        }
+                    ]
+                }
+
+            else:
+                new_entries[entry["Name"]]["SubEntries"] += [{"Start": entry["Start"], "End": entry["End"]}]
+
+        new_tracker_entries[date] = [entry for entry in new_entries.values()]
+
+    data["TrackerEntries"] = new_tracker_entries
+    data["Verion"] = "v1_0_1"
+    return data
+
+if __name__ == "__main__":
+    data_file_path: str = os.path.join(
+        str(os.getenv("LOCALAPPDATA")),
+        "Microsoft\\PowerToys\\PowerToys Run\\Settings\\Plugins\\Community.PowerToys.Run.Plugin.TimeTracker",
+        "data.json"
+    )
+
+    print(data_file_path)
+
+    if (not os.path.isfile(data_file_path)):
+        print("No data-file found. Migration aborted.\n")
+        input("Press ENTER to continue...")
+        sys.exit()
+
+    with open(data_file_path, "r") as data_file:
+        data = json.loads(data_file.read())
+
+    print(f"Data-file found and read. Current version: {data["Version"]}\nStarting migration...\n")
+
+    migration_complete: bool = False
+
+    while (not migration_complete):
+        match (data["Version"]):
+            case "v1_0_0":
+                print(f"Migrating to version 1.0.1...")
+                data = migrateToV1_0_1(data)
+                break
+            case _:
+                print("No further migration needed.")
+                migration_complete = True
+                break
+
+    with open(data_file_path, "w") as data_file:
+        data_file.write(json.dumps(data))
+
+    print("\nMigration done.\n")
+    input("Press ENTER to continue...")
